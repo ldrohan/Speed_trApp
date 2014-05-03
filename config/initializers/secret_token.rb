9c331e5cc0eb987872271668033c01f8c0402c13
ldrohan <drohanluke@gmail.com>
2014-05-03 12:55:10 -0700

    First commit on speed_trapp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SpeedTrapp::Application.config.secret_key_base = '4a7bfa3d6fdc838493d0c33f167304bf04b58ec57d66d3f5be42b7228b13f297310ad126e0686af869dfc4b8adcd89cb1a8f85fce1b85b27bb8639f37365d843'
