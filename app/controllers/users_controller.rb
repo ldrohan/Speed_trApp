78ee4bcdacfc9bef27afc7a11d2136f28a33d3ce
ldrohan <drohanluke@gmail.com>
2014-05-03 15:46:38 -0700

    Profile show page and method creadted

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,15 @@ class UsersController < ApplicationController
 	def new
 	  @user = User.new			  	
 	end	
+
+	def create
+		user = User.new(name: params[:user][:name], email: params[:user][:email], password: params[:user][:password], password_confirmation: params[:user][:password_confirmation])
+		user.save
+		redirect_to user
+		#creates new user and redirects to their profile page
+	end	
+
+	def show
+		@user = User.find(params[:id])
+	end	
 end
