a4bcaeaa1134766968c8600b7f9befc17e814e14
ldrohan <drohanluke@gmail.com>
2014-05-03 14:30:20 -0700

    Adding rspec and user model test

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+# describe User do
+#   it 'should have a name, email, password, and password confirmation' do
+#   	user = User.create(name: nil, email: nil, password:'passwordbob', password_confirmation: 'passwordbob')
+#   	user.should_not 
+#   end	
+end
