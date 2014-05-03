91d0505e51982b920a5d2d2f5df33a500f22cd7c
ldrohan <drohanluke@gmail.com>
2014-05-03 14:14:04 -0700

    Adding User model, migration and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(name:'Brita', email:'brita@gmail.com', password:'password456', password_confirmation:'password456')
+User.create(name:'Sarah', email:'sarah@gmail.com', password:'passwordsarah', password_confirmation:'passwordsarah')
+User.create(name:'Jim', email:'jim@gmail.com', password:'passwordjim', password_confirmation:'passwordjim')
