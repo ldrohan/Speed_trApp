81f994dfd60fa92ba54cbc628ddd5d30ed222409
ldrohan <drohanluke@gmail.com>
2014-05-05 14:55:12 -0700

    map working

--- app/controllers/traps_controller.rb
+++ app/controllers/traps_controller.rb
@@ -1,5 +1,16 @@
 class TrapsController < ApplicationController
 	def new
-		@trap = Trap.new
+      @trap = Trap.new
+	end	
+      
+	def create
+      # @trap = Trap.new(trap_params)
+      #   respond_to do |format|
+      #     if @trap.save
+      #     	format.html { redirect_to @trap, notice: 'Your trap was created successfully'}
+      #     	format.js { }
+      #     else
+      #       redirect_to '/traps/new'
+      #     end  		
 	end	
 end
