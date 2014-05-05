25b54fa072457e3e1c3c7b18f40f8ea44c6a83bb
ldrohan <drohanluke@gmail.com>
2014-05-05 10:26:05 -0700

    removing migration file

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SpeedTrapp::Application.config.secret_key_base = '4a7bfa3d6fdc838493d0c33f167304bf04b58ec57d66d3f5be42b7228b13f297310ad126e0686af869dfc4b8adcd89cb1a8f85fce1b85b27bb8639f37365d843'
+SpeedTrapp::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
